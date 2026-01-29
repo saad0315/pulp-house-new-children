30db82a515a248a1e6f30a8e5127f72fe65dcc59
saad0315 <158080391+saad0315@users.noreply.github.com>
2026-01-29 06:19:22 +0500

    finalized code

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
-import { CheckCircle2, ArrowRight } from "lucide-react";
+import { m } from "framer-motion";
+import { CheckCircle2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Navbar from "@/components/layout/Navbar";
